7c823381a669f8ade9d54e632b5a3c4b54bb2557
mithsin <hsinbluemoon@gmail.com>
2017-12-22 11:29:34 -0500

    make it

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
